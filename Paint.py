ba9b3fc01bfd849009042ee498f3a519bc67bc6f
Leonardo7250 <73494910+Leonardo7250@users.noreply.github.com>
2020-10-28 12:14:53 -0600

    Revert "T y R (#1)"
    This reverts commit 7410b422d2c367edafab4ad7522b765fc7e262e9.

--- Paint.py
+++ Paint.py
@@ -21,34 +21,20 @@ def square(start, end):
 
     end_fill()
 
-def circle(start, end):
+def drawcircle(start, end):
     "Draw circle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    circle((end.y - start.y)/2,360)
 
 def rectangle(start, end):
     "Draw rectangle from start to end."
-    up()
-    goto(start.x, start.y)
-    down()
-    begin_fill()
-    for i in range (2):
-        forward(end.x - start.x)
-        left(90)
-        forward((1/2)*(end.x - start.x))
-        left(90)
-    end_fill()
+    pass  # TODO
+
 def triangle(start, end):
     "Draw triangle from start to end."
-    up()
-    goto(start.x, start.y)
-    down()
-    begin_fill()
-    for i in range (3):
-        forward(end.x - start.x)
-        left(120)
-     end_fill()   
-    
-    
+    pass  # TODO
 
 def tap(x, y):
     "Store starting point or draw shape."
@@ -76,10 +62,10 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
-onkey(lambda: color('purple'), 'P')
+onkey(lambda: color('magenta'), 'M')  
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
-onkey(lambda: store('shape', circle), 'c')
+onkey(lambda: store('shape', drawcircle), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
 done()
